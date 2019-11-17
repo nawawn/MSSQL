73d3f08dd13d0383bc95c338a5f18c39ae8a3680
nawawn <nawawn@googlemail.com>
2019-11-17 22:02:16 +0000

    updated script

--- SqlCheck/SqlCheck.ps1
+++ SqlCheck/SqlCheck.ps1
@@ -9,7 +9,6 @@ Function Start-PreflightCheck{
         [String]$ComputerName,
         [Switch]$StopService
     )
-    <#
     DynamicParam{
         If ($StopService){
             $Attrib = New-Object System.Management.Automation.ParameterAttribute
@@ -28,14 +27,20 @@ Function Start-PreflightCheck{
             return $ParamDictionary
         }
     }
-
-    $ReportPath = "\\FileServer\reports\PreFlightChecks"
-    $CsvPath    = "C:\temp"
-    If (-Not(Test-Path -path $ReportPath)){
-        Write-Warning "$ReportPath Path not found"
-        Exit 1
+    Begin{
+        $ReportPath = "\\FileServer\reports\PreFlightChecks"
+        $CsvPath    = "\\FileServer\reports\CSV"
     }
-    #>
+    Process{
+        If (-Not(Test-Path -path $ReportPath)){
+            Write-Warning "$ReportPath Path not found"
+            Exit 1
+        }
+        If (-Not(Test-Path -path $CsvPath)){
+            Write-Warning "$CsvPath Path not found"
+            Exit 1
+        }
+    }    
 }
 Function Get-DriveSpace{
     [CmdletBinding()]
@@ -271,11 +276,11 @@ Function Stop-SQLServices{
     Process{
         Write-Verbose "Loading SQL Management Object..."
         If ([Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")){            
-            [void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO')
-            [void][Reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.SqlWmiManagement")
+            [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMO")
+            [void][System.Reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.SqlWmiManagement")
             
-            $SqlServer = New-Object Microsoft.SqlServer.Management.Smo.Server($ComputerName)
-            $SqlConf   = New-Object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer $SqlServer.ComputerNamePhysicalNetBIOS            
+            $SqlServer  = New-Object Microsoft.SqlServer.Management.Smo.Server($ComputerName)
+            $SqlConfMgr = New-Object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer $SqlServer.ComputerNamePhysicalNetBIOS            
         }    
         Else {         
             Write-Warning "Please install MS SQL Management Studio"   
@@ -283,9 +288,9 @@ Function Stop-SQLServices{
         }
 
         Write-Verbose "$ComputerName - Stopping SQL Services..."
-        $SqlConf.Services | Where-Object{$_.ServiceState -eq 'Running'} | ForEach-Object{$_.stop()}    
+        $SqlConfMgr.Services | Where-Object{$_.ServiceState -eq 'Running'} | ForEach-Object{$_.stop()}    
         If ($Wait){
-            $SrvName = $SqlConf.Services | Where-Object{$_.ServiceState -eq 'Running'} | Select-Object -ExpandProperty Name
+            $SrvName = $SqlConfMgr.Services | Where-Object{$_.ServiceState -eq 'Running'} | Select-Object -ExpandProperty Name
             Do {
                 [Bool]$IsRunning = (($SrvName | Get-Service -ComputerName $ComputerName | Where-Object{$_.Status -eq "Running"}).count -gt 0)
                 Write-Verbose "Services being stopped? $IsRunning"
@@ -319,35 +324,33 @@ Function Start-SQLServices{
         [System.IO.FileInfo]$MasterList = '\\FileServer\reports\Scripts\MasterList.psd1'
     )
     Begin {
-        $CsvFile = "$CSVPath\$ComputerName.csv"
         $RunningSrv = @()
-        If (Test-Path $CsvFile){
-            $Csv = Import-Csv $CsvFile            
-            $RunningSrv = ($Csv | Where-Object{$_.ServiceState -eq "Running"} | Select-Object -ExpandProperty Name)            
-        }
-        Elseif(Test-Path $MasterList){            
-            $ServiceList = Import-PowerShellDataFile $MasterList
-            $RunningSrv = $($ServiceList.$ComputerName.Running.Name)
-            
-            If ($ServiceName){
-                $ServiceList.$ComputerName.Running.Name | Get-Service -ComputerName | Start-Service
-            }
-            Else {
-                Write-Warning "Unable to obtain running services from the Master Service List for $ComputerName!"
-            }
-        }
-        Else{
-            Write-Warning "Please manually restart the Sql services or reboot the server!"
-        }
+        $SrvToStart = @()
+        $CsvFile    = "$CSVPath\$ComputerName.csv"        
     }
     Process {
-        If ($CsvRunningSrv){
-            Write-Verbose "Starting services according to running state in CSV..."       
-            $CsvRunningSrv | Get-Service -ComputerName $ComputerName | Start-Service
+        If (Test-Path -Path $CsvFile){
+            $Csv = Import-Csv -Path $CsvFile            
+            $RunningSrv = ($Csv | Where-Object{$_.ServiceState -eq "Running"} | Select-Object -ExpandProperty Name)            
+        }
+        Elseif(Test-Path -Path $MasterList){            
+            $ServiceList = Import-PowerShellDataFile -Path $MasterList
+            $RunningSrv = $($ServiceList.$ComputerName.Running.Name)            
+        }
+        Else{
+            Write-Warning "Unable to obtain running services details from both CSV and PSD1 file!"
+        }
+        If ($RunningSrv){
+            Write-Verbose "Starting services according to previous running state..."       
+            #$RunningSrv | Get-Service -ComputerName $ComputerName | Start-Service
+            Foreach($Service in $RunningSrv){
+                If (-Not(Test-ServiceStatus -Name $Service -Status 'Running' -ComputerName $ComputerName)){
+                    $SrvToStart += $Service
+                }
+            }
+            $SrvToStart | Get-Service -ComputerName $ComputerName | Start-Service
         } 
-        Else{            
-            Write-Warning "Unable to find the CSV file with Services info"
-            Write-Warning "The start-up process for services is ignored."
+        Else{
             Write-Warning "Please reboot the server, if the problem persists."
         }
     }
@@ -375,11 +378,11 @@ Function Start-SQLServices1{
     Process{
         Write-Verbose "Loading SQL Management Object..."
         If ([Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")){            
-            [void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO')
-            [void][Reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.SqlWmiManagement")
+            [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMO")
+            [void][System.Reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.SqlWmiManagement")
             
-            $SqlServer = New-Object Microsoft.SqlServer.Management.Smo.Server($ComputerName)
-            $SqlConf = New-Object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer $SqlServer.ComputerNamePhysicalNetBIOS            
+            $SqlServer  = New-Object Microsoft.SqlServer.Management.Smo.Server($ComputerName)
+            $SqlConfMgr = New-Object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer $SqlServer.ComputerNamePhysicalNetBIOS            
         }    
         Else {         
             Write-Warning "Please install MS SQL Management Studio"   
@@ -387,7 +390,7 @@ Function Start-SQLServices1{
         }
         If ($CsvRunningSrv){
             Write-Verbose "Starting services which have running state in CSV..."       
-            $SqlConf.Services | 
+            $SqlConfMgr.Services | 
                 Where-Object{($_.ServiceState -eq "Stopped") -and ($CsvRunningSrv -contains $_.Name)} | 
                 ForEach-Object{$_.start()}
                 #Select-Object Name,ServiceState
@@ -403,9 +406,9 @@ Function Start-SQLServices1{
         #### This needs to be fixed....  
         Else{            
             Write-Verbose "Starting all services with StartMode set to Auto..."
-            $SqlConf.Services | Where-Object{$_.ServiceState -eq "Stopped" -and $_.StartMode -eq "Auto"} | ForEach-Object{$_.start()}
+            $SqlConfMgr.Services | Where-Object{$_.ServiceState -eq "Stopped" -and $_.StartMode -eq "Auto"} | ForEach-Object{$_.start()}
             If ($Wait){            
-                $SrvName = $SqlConf.Services | Where-Object{$_.ServiceState -eq "Stopped" -and $_.StartMode -eq "Auto"} | Select-Object -ExpandProperty Name
+                $SrvName = $SqlConfMgr.Services | Where-Object{$_.ServiceState -eq "Stopped" -and $_.StartMode -eq "Auto"} | Select-Object -ExpandProperty Name
                 Do {
                     [Bool]$IsStopped = (($SrvName | Get-Service -ComputerName $ComputerName | Where-Object{$_.Status -eq "Stopped"}).Count -gt 0)
                     Write-Verbose "Services being started? $IsStopped"
