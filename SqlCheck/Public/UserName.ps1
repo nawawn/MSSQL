6a94c5ca0aa3ccf1690a4f2ed7b698d35eca0d0a
Naw Awn <nawawn@googlemail.com>
2019-11-18 18:35:31 +0000

    updated UserName script

--- SqlCheck/Public/UserName.ps1
+++ SqlCheck/Public/UserName.ps1
@@ -1,17 +1,41 @@
 Function Get-UserName{
+<#
+.Synopsis
+   Translate user sid into user name
+.DESCRIPTION
+   The cmdlet translates the user's security identifier to the name
+.EXAMPLE
+   Get-UserName -UserSID 'S-1-5-21-Some-Number-here-0000'
+.INPUTS
+   A User Security Identifier (SID)
+.OUTPUTS
+   User Name
+#>    
     [CmdletBinding()]
     Param(
         [ValidateNotNullOrEmpty()]
         [String]$UserSID
     )
     Process{
-        $objSID = New-Object System.Security.Principal.SecurityIdentifier ($UserSID)
+        $objSID = New-Object System.Security.Principal.SecurityIdentifier($UserSID)
         $objUser = $objSID.Translate( [System.Security.Principal.NTAccount])
         $objUser.Value
     }
 }
 
 Function Get-UserSID{
+<#
+.Synopsis
+   Translate user name to user sid
+.DESCRIPTION
+   The cmdlet translates the user's name to the security identifier
+.EXAMPLE
+   Get-UserName -Username 'Naw.Awn'
+.INPUTS
+   User Name
+.OUTPUTS
+   A User Security Identifier (SID)
+#> 
     [CmdletBinding()]
     Param(
         [ValidateNotNullOrEmpty()]
